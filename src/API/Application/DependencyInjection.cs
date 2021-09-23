b823f669db3dcf4daa78983ff578af4d36aa6a5b
Patrick Ratzow <pr@tigermedia.dk>
2021-09-23 11:36:43 +0200

    feat: add Application project

---
+++ src/API/Application/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Commentor.GivEtPraj.Application.Common.Behaviors;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Commentor.GivEtPraj.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddFluentValidation();
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
