c5d8466077e838fd2291577fc30433274ce695f2
Patrick Ratzow <pr@tigermedia.dk>
2021-09-23 12:27:58 +0200

    feat(db): add example configuration for Case

---
+++ src/API/Infrastructure.Persistence/Configurations/CaseConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Commentor.GivEtPraj.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations
+{
+    public class CaseConfiguration : IEntityTypeConfiguration<Case>
+    {
+        public void Configure(EntityTypeBuilder<Case> builder)
+        {
+            builder.Property(c => c.Title)
+                .HasMaxLength(127)
+                .IsRequired();
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(4096)
+                .IsRequired();
+        }
+    }
+}
