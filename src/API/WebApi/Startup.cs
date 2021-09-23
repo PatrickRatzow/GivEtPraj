ee9911a548be027350b478e9719933279c44737c
Patrick Ratzow <pr@tigermedia.dk>
2021-09-23 09:59:18 +0200

    feat: add WebApi project

---
+++ src/API/WebApi/Startup.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+
+namespace Commentor.GivEtPraj.WebApi
+{
+    public class Startup
+    {
+        private static readonly ILoggerFactory Logger = LoggerFactory.Create(builder =>
+        {
+            builder.AddConsole();
+        });
+
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddApplication();
+            //services.AddInfrastructure(_configuration, _env);
+
+            services.AddCors();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new()
+                {
+                    Title = "Despawn.API",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new()
+                {
+                    Description =
+                        "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new()
+                {
+                    {
+                        new()
+                        {
+                            Reference = new()
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Commentor.GivEtPraj.API v1"));
+
+                // app.UseRedisInformation();
+
+                app.UseCors(options =>
+                    options.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                );
+            }
+            else
+            {
+                // Handle CORS for prod
+            }
+
+            //app.UseFluentValidationExceptionHandler();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            //app.UseApplication();
+        }
+    }
+}
