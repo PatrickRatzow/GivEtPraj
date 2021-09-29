5daf25663968d4eec6d56d5116400e305bbee74d
Br0der99 <1083570@ucn.dk>
2021-09-29 14:30:39 +0200

    feat(category): fetch all categories from db

---
+++ src/API/Application/Categories/Queries/FindAllCategoriesQuery.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commentor.GivEtPraj.Application.Categories.Queries;
+
+public record FindAllCategoriesQuery : IRequest<List<CaseCategoryDto>>;
+
+public class FindAllCategoriesQueryHandler : IRequestHandler<FindAllCategoriesQuery, List<CaseCategoryDto>>
+{
+    private readonly IAppDbContext _db;
+    private readonly IMapper _mapper;
+
+    public FindAllCategoriesQueryHandler(IAppDbContext db, IMapper mapper)
+    {
+        _db = db;
+        _mapper = mapper;
+    }
+
+    public async Task<List<CaseCategoryDto>> Handle(FindAllCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await _db.Categories.ToListAsync(cancellationToken);
+
+        return _mapper.Map<List<Category>, List<CaseCategoryDto>>(categories);
+    }
+}
---
+++ src/API/WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using Commentor.GivEtPraj.Application.Categories.Queries;
+
+namespace Commentor.GivEtPraj.WebApi.Controllers;
+
+[ApiController]
+[Route("v1/categories")]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> FindAllCategories(CancellationToken cancellationToken)
+    {
+        var query = new FindAllCategoriesQuery();
+        var result = await _mediator.Send(query, cancellationToken);
+
+        return Ok(result);
+    }
+}
