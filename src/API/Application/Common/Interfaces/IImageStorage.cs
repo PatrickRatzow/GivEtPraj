7c2a9653b2dcf5600e83ddec0d62ede94c31111f
Patrick Ratzow <pr@tigermedia.dk>
2021-09-29 10:55:00 +0200

    feat(storage): create ImageStorage
    Uses IFileStorage under the hood for, but abstracts the image concerns

---
+++ src/API/Application/Common/Interfaces/IImageStorage.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Commentor.GivEtPraj.Application.Common.Interfaces;
+
+public interface IImageStorage
+{
+    Task<Stream?> FindImage(string name);
+    Task<bool> UploadImage(string name, Stream content);
+}
---
+++ src/API/Infrastructure/Storage/ImageStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Commentor.GivEtPraj.Application.Common.Interfaces;
+
+namespace Commentor.GivEtPraj.Infrastructure.Storage;
+
+public class ImageStorage : IImageStorage
+{
+    private readonly IFileStorage _fileStorage;
+
+    public ImageStorage(IFileStorage fileStorage)
+    {
+        _fileStorage = fileStorage;
+    }
+
+    public async Task<Stream?> FindImage(string name)
+        => await _fileStorage.FindFile(FullPath(name));
+
+    public async Task<bool> UploadImage(string name, Stream content)
+    {
+        var extension = Path.GetExtension(name);
+        var contentType = extension switch
+        {
+            "jpg" => "image/jpeg",
+            "jpeg" => "image/jpeg",
+            "png" => "image/png",
+            _ => throw new ArgumentException($"{name} is not an allowed file extension")
+        };
+        
+        return await _fileStorage.UploadFile(FullPath(name), content, contentType);
+    }
+
+    private static string FullPath(string name) => $"cases/{name}";
+}
