221a10b2107fb37dc9e60540722d2c9f5a110c32
SimonNiese <1086234@ucn.dk>
2021-10-04 14:49:41 +0200

    feat(compression): Added compression to uploaded images.

--- src/API/Application/Cases/Commands/CreateCaseCommand.cs
+++ src/API/Application/Cases/Commands/CreateCaseCommand.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace Commentor.GivEtPraj.Application.Cases.Commands;
 
@@ -52,19 +54,31 @@ public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, OneOf
 
     private async Task<List<Picture>> CreateImages(CreateCaseCommand request)
     {
-        var images = request.Images
-            .Select(i => new Picture
-            {
-                Id = Guid.NewGuid()
-            })
-            .ToList();
+        var pictures = new List<Picture>();
+        var list = new List<(string Image, Guid Id)>();
+        foreach (var image in request.Images)
+        {
+            var guid = Guid.NewGuid();
+            list.Add((image, guid));
+            pictures.Add(new Picture { Id = guid });
+        }
 
-        await UploadImages(images);
 
-        return images;
+        var compressedList = new List<(string Image, Guid Id)>();
+        foreach(var item in list)
+        {
+            MemoryStream img = VaryQualityLevel(Base64ToImage(item.Image));
+
+            compressedList.Add((Convert.ToBase64String(img.ToArray()), item.Id));
+
+        }
+
+        await UploadImages(compressedList);
+
+        return pictures;
     }
 
-    private async ValueTask UploadImages(IReadOnlyList<Picture> images)
+    private async ValueTask UploadImages(IReadOnlyList<(string Image, Guid Id)> images)
     {
         if (!images.Any()) return;
 
@@ -76,7 +90,7 @@ public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, OneOf
             disposables.Add(stream);
             disposables.Add(writer);
 
-            writer.Write(images[index]);
+            writer.Write(cp.Image);
             writer.Flush();
 
             stream.Position = 0;
@@ -88,6 +102,65 @@ public class CreateCaseCommandHandler : IRequestHandler<CreateCaseCommand, OneOf
         {
             await disposable.DisposeAsync();
         }
+    }
+
+    private static Image Base64ToImage(string base64String)
+    {
+        // Convert base 64 string to byte[]
+        byte[] imageBytes = Convert.FromBase64String(base64String);
+        // Convert byte[] to Image
+        using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+        {
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
+    }
+
+    private MemoryStream VaryQualityLevel(Image img)
+    {
+        // Get a bitmap. The using statement ensures objects  
+        // are automatically disposed from memory after use.  
+        using (Bitmap bmp1 = new Bitmap(img))
+        {
+            ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+
+            // Create an Encoder object based on the GUID  
+            // for the Quality parameter category.  
+            System.Drawing.Imaging.Encoder myEncoder =
+                System.Drawing.Imaging.Encoder.Quality;
+
+            // Create an EncoderParameters object.  
+            // An EncoderParameters object has an array of EncoderParameter  
+            // objects. In this case, there is only one  
+            // EncoderParameter object in the array.  
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+            MemoryStream CompressedImg = new MemoryStream(); 
+
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, 50L);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            bmp1.Save(CompressedImg, jpgEncoder, myEncoderParameters);
+
+                       
+            
+
+            
+
+            return CompressedImg;
+        }
+    }
+
+    private ImageCodecInfo GetEncoder(ImageFormat format)
+    {
+        ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+        foreach (ImageCodecInfo codec in codecs)
+        {
+            if (codec.FormatID == format.Guid)
+            {
+                return codec;
+            }
+        }
+        return null;
     }
 }
 
