d750b573a93d40ab57fd4049e44e096677a4dc97
SimonNiese <1086234@ucn.dk>
2021-10-12 09:13:41 +0200

    feat(domain): add new classes to domain

---
+++ src/API/Domain/Entities/CaseUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Commentor.GivEtPraj.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commentor.GivEtPraj.Domain.Entities
+{
+    public class CaseUpdate
+    {
+        public DateTime CreatedAt { get; set; }
+        public Status CurrentStatus { get; set; }
+        public Employee Employee { get; set; }
+        public bool SendToReporter {  get; set; }
+    }
+}
