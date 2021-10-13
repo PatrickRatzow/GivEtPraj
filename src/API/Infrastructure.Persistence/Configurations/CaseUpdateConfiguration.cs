43be748e0087bec7d78cbd09e1e80c1527cdea1a
SimonNiese <1086234@ucn.dk>
2021-10-13 13:02:12 +0200

    feat(domain): Add Configurations to new classes in Domain.

---
+++ src/API/Infrastructure.Persistence/Configurations/CaseUpdateConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Persistence.Configurations;
+internal class CaseUpdateConfiguration : IEntityTypeConfiguration<CaseUpdate>
+{
+    public void Configure(EntityTypeBuilder<CaseUpdate> builder)
+    {
+
+        builder.Property(c => c.CreatedAt)
+            .IsRequired();
+
+        builder.Property(c => c.CurrentStatus);
+
+        builder.OwnsOne(c => c.Employee);
+
+        builder.Property(c => c.SendToReporter);
+
+    }
+}
+
