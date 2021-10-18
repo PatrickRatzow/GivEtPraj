fe428822e676126400671276e889f5d05dd962cb
Patrick Ratzow <pr@tigermedia.dk>
2021-10-18 12:48:21 +0200

    feat(vue): added capacitor

--- src/Web/PublicApp/src/views/create-case/Map.vue
+++ src/Web/PublicApp/src/views/create-case/Map.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "../../store";
+import { Geolocation } from "@capacitor/geolocation";
 
 const store = useStore();
 const caseInCreation = computed(() => store.state.caseInCreation);
+
+const loc = ref<{
+  lat: null | number;
+  long: null | number;
+}>({
+  lat: null,
+  long: null,
+});
+
+const getCurrentPosition = async () => {
+  const pos = await Geolocation.getCurrentPosition();
+  loc.value = {
+    lat: pos.coords.latitude,
+    long: pos.coords.longitude,
+  };
+};
 </script>
 
 <template>
@@ -14,6 +31,15 @@ const caseInCreation = computed(() => store.state.caseInCreation);
       <br />
 
       {{ caseInCreation.category }}
+
+      <div>
+        <h1>Geolocation</h1>
+        <p>Your location is:</p>
+        <p>Latitude: {{ loc.lat }}</p>
+        <p>Longitude: {{ loc.long }}</p>
+
+        <button @click="getCurrentPosition">Get Current Location</button>
+      </div>
     </h1>
     <div v-else>
       <button @click="store.commit('startCaseCreation')">start</button>
