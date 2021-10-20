b7f2dd18d0d068df20dee6f1c41114894efca08d
SleepyC4 <nikolajcarlslund@gmail.com>
2021-10-20 16:06:58 +0200

    feat: added CategoryItem

--- src/Web/PublicApp/src/router/index.ts
+++ src/Web/PublicApp/src/router/index.ts
@@ -3,12 +3,14 @@ import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Map from "../views/create-case/Map.vue";
 import Camera from "../views/Camera.vue";
+import Category from "../views/create-case/CategoryItem.vue";
 
 const routes = [
 	{ path: "/", name: "Home", component: Home },
 	{ path: "/about", name: "About", component: About },
 	{ path: "/create-case/map", name: "Map", component: Map },
 	{ path: "/camera", name: "Camera", component: Camera },
+	{ path: "/category", name: "Category", component: Category },
 ];
 
 const router = createRouter({
