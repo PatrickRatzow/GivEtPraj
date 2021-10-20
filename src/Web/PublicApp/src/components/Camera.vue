69de637e0289d7dfa5723755119e69d03469a59f
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-10-20 20:06:50 +0200

    fix(vue): add missing vuex -> pinia conversion

--- src/Web/PublicApp/src/components/Camera.vue
+++ src/Web/PublicApp/src/components/Camera.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { Camera, Photo, CameraResultType } from "@capacitor/camera";
-import { computed, ref } from "vue";
+import { Camera, CameraResultType } from "@capacitor/camera";
 import axios from "../utils/axios";
 import { useCreateCaseStore } from "@/stores";
 
 const caseStore = useCreateCaseStore();
+
 const takePicture = async () => {
   const image = await Camera.getPhoto({
     quality: 90,
@@ -12,14 +12,19 @@ const takePicture = async () => {
     resultType: CameraResultType.Base64,
   });
 
-  store.commit("addImage", image);
+  caseStore.$patch((state) => {
+    state.images.push(image);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (state as any).hasChanged = true;
+  });
 };
+
 interface CameraRequest {
   images: string[];
 }
 
 const uploadPictures = async (): Promise<boolean> => {
-  const data: CameraRequest = { images: images.value.map((i) => i.base64String as string) };
+  const data: CameraRequest = { images: caseStore.images.map((i) => i.base64String as string) };
   const resp = await axios.post("camera", data);
 
   return resp.status === 200;
@@ -29,7 +34,7 @@ const uploadPictures = async (): Promise<boolean> => {
   <div class="container">
     <div class="image-content">
       <button @click="takePicture">A button!</button>
-      <img v-for="(image, i) in images" :key="i" :src="`data:image/jpeg;base64,${image.base64String}`" />
+      <img v-for="(image, i) in caseStore.images" :key="i" :src="`data:image/jpeg;base64,${image.base64String}`" />
       <button @click="uploadPictures">A button!</button>
     </div>
   </div>
