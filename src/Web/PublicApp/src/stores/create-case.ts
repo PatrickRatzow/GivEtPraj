0910b88ffd8bbef024d0d9ab032084cff0e9e98c
SleepyC4 <nikolajcarlslund@gmail.com>
2021-10-21 15:55:08 +0200

    feat(vue): added functionality for subcategory page

--- src/Web/PublicApp/src/stores/create-case.ts
+++ src/Web/PublicApp/src/stores/create-case.ts
@@ -3,7 +3,7 @@ import { defineStore } from "pinia";
 
 export const useCreateCaseStore = defineStore("createCase", () => {
 	const category = ref<Category | null>(null);
-	const subCategories = ref<string[]>([]);
+	const subCategories = ref<SubCategory[]>([]);
 	const images = reactive<Photo[]>([]);
 	const comment = ref<string | undefined>(undefined);
 
