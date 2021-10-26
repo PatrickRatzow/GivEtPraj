770e960468b9f59ef9f31b5434a64ad213ee7218
Patrick Ratzow <pr@tigermedia.dk>
2021-10-26 13:47:04 +0200

    feat(vue): reroute to url when 404

--- src/Web/PublicApp/src/pages/404.vue
+++ src/Web/PublicApp/src/pages/404.vue
@@ -1,3 +1,14 @@
+<script lang="ts">
+export default {
+  created() {
+    const url = "/404-not-found";
+    if (window.location.pathname === url) return;
+
+    window.location.pathname = url;
+  },
+};
+</script>
+
 <template>
   <ion-page fullscreen class="flex justify-center items-center">
     <h1 class="text-2xl text-black mt-8 dark:text-white">404 - Ain't nothin here</h1>
