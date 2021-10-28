acb1c13b857d723d49fe49b3710e0abc021b04b2
Patrick Ratzow <pr@tigermedia.dk>
2021-10-28 09:10:39 +0200

    refactor(vue): make pinia a module

--- src/Web/PublicApp/src/main.ts
+++ src/Web/PublicApp/src/main.ts
@@ -2,14 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/main.css";
 import router from "./router";
-import { createPinia } from "pinia";
-
-const pinia = createPinia();
-pinia.state.value = {};
 
 const app = createApp(App);
 app.use(router);
-app.use(pinia);
 
 /* Load all modules */
 const modules = Object.values(import.meta.globEager("./modules/*.ts")).map((i) => i.install?.(app));
