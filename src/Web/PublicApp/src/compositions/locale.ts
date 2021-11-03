42a6de4da80b85cb5227b8127c8d193064a34413
InvisibleOlive <70630509+InvisibleOlive@users.noreply.github.com>
2021-11-03 13:35:00 +0100

    feat(vue): added offline map functionality and popups for any errors

--- src/Web/PublicApp/src/compositions/locale.ts
+++ src/Web/PublicApp/src/compositions/locale.ts
@@ -2,7 +2,7 @@ import { Device } from "@capacitor/device";
 
 export function useLocale() {
 	const language = ref<string>();
-	
+
 	async function getLanguageCode(): Promise<Language> {
 		const languageCode = await Device.getLanguageCode();
 		if (languageCode.value == "da") return "da";
--- src/Web/PublicApp/src/pages/create-case/map.vue
+++ src/Web/PublicApp/src/pages/create-case/map.vue
@@ -3,6 +3,8 @@ import { useNetwork } from "@/compositions/network";
 import { useLocale } from "@/compositions/locale";
 import { useCreateCaseStore } from "@/stores/create-case";
 import { Geolocation, WatchPositionCallback } from "@capacitor/geolocation";
+import { presentAlert } from "@/compositions/GeolocationErrorAlert";
+
 import {
   map,
   tileLayer,
@@ -37,7 +39,7 @@ const loadMap = async () => {
       accessToken: "pk.eyJ1Ijoic2ltb25uaWVzZSIsImEiOiJja3ZnZG9yZm0wMWxzMnVwM3Nlcjk3YmpiIn0.p1TDwifWqx1kcYFnBqI_fg",
       bounds: [
         [57.765923, 7.482181],
-        [54.546490, 15.455954],
+        [54.54649, 15.455954],
       ],
     });
     const satellite = tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -51,7 +53,7 @@ const loadMap = async () => {
       accessToken: "pk.eyJ1Ijoic2ltb25uaWVzZSIsImEiOiJja3ZnZG9yZm0wMWxzMnVwM3Nlcjk3YmpiIn0.p1TDwifWqx1kcYFnBqI_fg",
       bounds: [
         [57.765923, 7.482181],
-        [54.546490, 15.455954],
+        [54.54649, 15.455954],
       ],
     });
 
@@ -122,6 +124,27 @@ const getStatus = () => network.status.value?.connected;
 watch(getStatus, loadMap);
 
 const isPositionValid = computed(() => createCase.geographicLocation);
+
+const sendInCurrentLocation = async () => {
+  try {
+    console.log("Start");
+    var pos = await Geolocation.getCurrentPosition({ enableHighAccuracy: true });
+    createCase.geographicLocation = { latitude: pos.coords.latitude, longitude: pos.coords.longitude };
+    router.push("/create-praj/category");
+  } catch (e: unknown) {
+    if (e instanceof GeolocationPositionError) {
+      if (e.code == 1) {
+        presentAlert(e.code);
+      } else if (e.code == 2) {
+        presentAlert(e.code);
+      } else if (e.code == 3) {
+        presentAlert(e.code);
+      }
+    }
+
+    console.log("Unknown error occurred");
+  }
+};
 </script>
 
 <template>
@@ -130,31 +153,48 @@ const isPositionValid = computed(() => createCase.geographicLocation);
       <ion-title>{{ t("create-case.map.title") }}</ion-title>
     </ion-toolbar>
     <ion-content>
-      <offline v-if="!getStatus()" class="h-full"></offline>
-      <div v-else id="mapid" class="h-full"></div>
-      <button
-        v-if="getStatus()"
-        class="
-          absolute
-          bottom-4
-          left-1/2
-          right-1/2
-          transform
-          -translate-x-1/2
-          py-2
-          px-4
-          z-[99999]
-          rounded-md
-          w-max
-          text-lg
-          transition-all
-        "
-        type="button"
-        :class="[isPositionValid ? ['bg-green-500 text-white'] : ['bg-gray-200 text-black border-red-500 opacity-90']]"
-        @click="router.push('/create-praj/category')"
-      >
-        {{ isPositionValid ? t("create-case.map.confirm-button.valid") : t("create-case.map.confirm-button.invalid") }}
-      </button>
+      <template v-if="!getStatus()">
+        <div class="flex flex-col justify-between items-center text-black dark:text-white text-center h-full">
+          <div class="flex flex-col items-center mt-8">
+            <i class="fas fa-exclamation-circle text-red-500 text-6xl mb-4"></i>
+            <h1 class="text-2xl">{{ t("create-case.map.internet-error.no-internet") }}</h1>
+            <p class="mx-8">{{ t("create-case.map.internet-error.no-internet-explanation") }}</p>
+          </div>
+
+          <ion-button class="mb-8" @click="sendInCurrentLocation">
+            {{ t("create-case.map.confirm-button-offline.valid") }}
+          </ion-button>
+        </div>
+      </template>
+      <template v-else>
+        <div id="mapid" class="h-full"></div>
+        <button
+          class="
+            absolute
+            bottom-4
+            left-1/2
+            right-1/2
+            transform
+            -translate-x-1/2
+            py-2
+            px-4
+            z-[99999]
+            rounded-md
+            w-max
+            text-lg
+            transition-all
+          "
+          type="button"
+          :class="[
+            isPositionValid ? ['bg-green-500 text-white'] : ['bg-gray-200 text-black border-red-500 opacity-90'],
+          ]"
+          @click="router.push('/create-praj/category')"
+        >
+          {{
+            isPositionValid ? t("create-case.map.confirm-button.valid") : t("create-case.map.confirm-button.invalid")
+          }}
+        </button>
+      </template>
     </ion-content>
   </ion-page>
 </template>
