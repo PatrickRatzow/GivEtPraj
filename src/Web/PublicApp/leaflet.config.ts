d6b1b72c4036468e9d19477cad8c316d83e9df7c
InvisibleOlive <70630509+InvisibleOlive@users.noreply.github.com>
2021-11-09 11:18:55 +0100

    refactor(vue): moved map and bounderies constants to config file

---
+++ src/Web/PublicApp/leaflet.config.ts
@@ -0,0 +1,61 @@
+import { TileLayerOptions } from "leaflet";
+
+export const streetMap: TileLayerOptions = {
+	attribution:
+		'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	minZoom: 6,
+	id: "mapbox/streets-v11",
+	tileSize: 512,
+	zoomOffset: -1,
+	accessToken: "pk.eyJ1Ijoic2ltb25uaWVzZSIsImEiOiJja3ZnZG9yZm0wMWxzMnVwM3Nlcjk3YmpiIn0.p1TDwifWqx1kcYFnBqI_fg",
+	bounds: [
+		[57.765923, 7.482181],
+		[54.54649, 15.455954],
+	],
+};
+
+export const sateliteMap: TileLayerOptions = {
+	attribution:
+		'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	minZoom: 6,
+	id: "mapbox/satellite-streets-v11",
+	tileSize: 512,
+	zoomOffset: -1,
+	accessToken: "pk.eyJ1Ijoic2ltb25uaWVzZSIsImEiOiJja3ZnZG9yZm0wMWxzMnVwM3Nlcjk3YmpiIn0.p1TDwifWqx1kcYFnBqI_fg",
+	bounds: [
+		[57.765923, 7.482181],
+		[54.54649, 15.455954],
+	],
+};
+
+export const bounderiesCoords = [
+	[
+		[
+			[54.783554, 8.025203],
+			[56.7008, 7.94616],
+			[57.209392, 8.586408],
+			[57.286365, 9.297795],
+			[57.950948, 10.293736],
+			[57.799633, 11.31339],
+			[56.031885, 12.661413],
+			[55.803497, 12.838052],
+			[55.50243, 12.822007],
+			[55.324403, 12.712139],
+			[54.5551, 12.744531],
+			[54.506417, 11.522005],
+			[54.805709, 9.818059],
+			[54.783554, 8.025203],
+		],
+	],
+	[
+		[
+			[55.319725, 14.763668],
+			[55.145379, 15.20082],
+			[54.962571, 15.098907],
+			[55.096303, 14.630914],
+			[55.319725, 14.763668],
+		],
+	],
+];
--- src/Web/PublicApp/src/pages/create-case/map.vue
+++ src/Web/PublicApp/src/pages/create-case/map.vue
@@ -2,23 +2,11 @@
 import { useNetwork } from "@/compositions/network";
 import { useLocale } from "@/compositions/locale";
 import { useCreateCaseStore } from "@/stores/create-case";
-import { Geolocation, WatchPositionCallback } from "@capacitor/geolocation";
-import { presentAlert } from "@/compositions/GeolocationErrorAlert";
+import { Geolocation } from "@capacitor/geolocation";
+import { presentAlert } from "@/compositions/geolocation-error-alert";
 import * as turf from "@turf/turf";
-import {
-  map,
-  tileLayer,
-  marker,
-  LeafletMouseEvent,
-  Marker,
-  control,
-  Control,
-  circle,
-  point,
-  LeafletEvent,
-  LayerGroup,
-  Map,
-} from "leaflet";
+import { streetMap, sateliteMap, bounderiesCoords } from "@/../leaflet.config";
+import { map, tileLayer, marker, LeafletMouseEvent, Marker, control, Control, circle, Map } from "leaflet";
 import { Position } from "@capacitor/geolocation/dist/esm/definitions";
 
 const router = useRouter();
@@ -28,35 +16,15 @@ const { t } = useI18n();
 const network = useNetwork();
 
 const loadMap = (): Map => {
-  let streets = tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 6,
-    id: "mapbox/streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: "pk.eyJ1Ijoic2ltb25uaWVzZSIsImEiOiJja3ZnZG9yZm0wMWxzMnVwM3Nlcjk3YmpiIn0.p1TDwifWqx1kcYFnBqI_fg",
-    bounds: [
-      [57.765923, 7.482181],
-      [54.54649, 15.455954],
-    ],
-  });
+  let streets = tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    streetMap
+  );
 
-  let satellite = tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    minZoom: 6,
-    id: "mapbox/satellite-streets-v11",
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: "pk.eyJ1Ijoic2ltb25uaWVzZSIsImEiOiJja3ZnZG9yZm0wMWxzMnVwM3Nlcjk3YmpiIn0.p1TDwifWqx1kcYFnBqI_fg",
-    bounds: [
-      [57.765923, 7.482181],
-      [54.54649, 15.455954],
-    ],
-  });
+  let satellite = tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    sateliteMap
+  );
 
   const myMap = map("mapid", { layers: [streets] }).setView([56.15674, 10.21076], 6);
   const addLayers = () => {
@@ -67,35 +35,7 @@ const loadMap = (): Map => {
     control.layers(baseMaps).addTo(myMap);
   };
 
-  let bounderies = turf.multiPolygon([
-    [
-      [
-        [54.783554, 8.025203],
-        [56.7008, 7.94616],
-        [57.209392, 8.586408],
-        [57.286365, 9.297795],
-        [57.950948, 10.293736],
-        [57.799633, 11.31339],
-        [56.031885, 12.661413],
-        [55.803497, 12.838052],
-        [55.50243, 12.822007],
-        [55.324403, 12.712139],
-        [54.5551, 12.744531],
-        [54.506417, 11.522005],
-        [54.805709, 9.818059],
-        [54.783554, 8.025203],
-      ],
-    ],
-    [
-      [
-        [55.319725, 14.763668],
-        [55.145379, 15.20082],
-        [54.962571, 15.098907],
-        [55.096303, 14.630914],
-        [55.319725, 14.763668],
-      ],
-    ],
-  ]);
+  let bounderies = turf.multiPolygon(bounderiesCoords);
 
   addLayers();
   watch(
@@ -153,7 +93,7 @@ const getUserPosition = async (map: Map): Promise<Position> => {
     userAccuracy.setRadius(newPos.coords.accuracy);
   };
 
-  const wait = Geolocation.watchPosition({ enableHighAccuracy: true, timeout: 10000 }, watchPos);
+  Geolocation.watchPosition({ enableHighAccuracy: true, timeout: 10000 }, watchPos);
 
   map.on("zoom", () => userPoint.setRadius(Math.pow(2, 20 - map.getZoom())));
 
