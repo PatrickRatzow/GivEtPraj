cf8ebb050628dccaee82ccd22a31e118e1ce4a3d
SleepyC4 <nikolajcarlslund@gmail.com>
2021-11-17 15:22:29 +0100

    feat(test): added two new tests for e2e

--- src/Web/PublicApp/tests/foo.spec.ts
+++ src/Web/PublicApp/tests/foo.spec.ts
@@ -13,7 +13,39 @@ test.describe("start up", () => {
 		expect(await button.count()).toBeFalsy();
 	});
 
-	test("click skip", async ({ page }) => {
-		await page.click("text=skip tutorial");
+	test("click map", async ({ page }) => {
+		await page.pause();
+		const divMap = "#mapid";
+		const button = page.locator("text=skip tutorial");
+
+		await button.click();
+		const map = await page.textContent(divMap);
+
+		expect(map).toBeTruthy();
+		await page.click(divMap);
+
+		expect(await page.isEnabled("button")).toBeTruthy();
+		await page.pause();
+	});
+
+	test("clicked grey area on map", async ({ page }) => {
+		await page.pause();
+		const divMap = "#mapid";
+		const button = page.locator("text=skip tutorial");
+
+		await button.click();
+		const map = await page.textContent(divMap);
+
+		expect(map).toBeTruthy();
+		//await page.click(divMap) -- Try to click in the grey area;
+		await page.click(divMap, {
+			position: {
+				x: 0,
+				y: 0,
+			},
+		});
+
+		expect(await page.isEnabled("button")).toBeFalsy();
+		await page.pause();
 	});
 });
