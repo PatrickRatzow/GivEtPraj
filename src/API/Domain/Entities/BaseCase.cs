7607cc12d0fe03a4e34d0059a9764b0378dc21d6
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-12-15 07:55:35 +0100

    refactor: remove basecase for now

--- src/API/Domain/Entities/BaseCase.cs
+++ src/API/Domain/Entities/BaseCase.cs
@@ -5,16 +5,7 @@ using FluentValidation;
 
 namespace Commentor.GivEtPraj.Domain.Entities;
 
-public class BaseCaseValidator : AbstractValidator<BaseCase>
-{
-    public BaseCaseValidator()
-    {
-        RuleFor(x => x.Category).NotNull();
-        RuleFor(x => x.Id).NotEmpty();
-    }
-}
-
-public abstract class BaseCase : BaseEntity
+public abstract class BaseCase
 {
     public int Id { get; set; }
     public Guid DeviceId { get; set; }
@@ -25,63 +16,4 @@ public abstract class BaseCase : BaseEntity
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset? UpdatedAt { get; set; }
     public List<CaseUpdate> CaseUpdates { get; set; } = null!;
-
-    public void AddImage(CaseImage image)
-    {
-        Images.Add(image);
-    }
-}
-
-public abstract class BaseEntity
-{
-    private static List<IValidator>? _validators;
-    private static List<IValidator> Validators 
-        => _validators ??= Assembly.GetExecutingAssembly()
-            .GetExportedTypes()
-            .Where(t => t.IsAssignableTo(typeof(IValidator)))
-            .Select(t => Activator.CreateInstance(t) as IValidator)
-            .Where(t => t is not null)
-            .ToList()!;
-
-    protected void Validate()
-    {
-        var type = GetType();
-        ValidateBaseClasses(type);
-        Validate(type);
-    }
-
-    private void ValidateBaseClasses(Type? type)
-    {
-        if (type is null) return;
-        if (type == typeof(BaseEntity)) return;
-
-        ValidateBaseClasses(type.BaseType);
-        Validate(type);
-    }
-
-    private void Validate(Type type)
-    {
-        var validator = FindValidators(type);
-        var context = new ValidationContext<object>(this);
-        var errors = validator
-            .Select(x => x.Validate(context))
-            .SelectMany(x => x.Errors)
-            .Where(x => x is not null)
-            .ToList();
-
-        if (errors.Any()) throw new ValidationException(errors);
-    }
-
-    private IEnumerable<IValidator> FindValidators(Type type)
-    {
-        return Validators
-            .Where(x =>
-            {
-                var interfaces = x.GetType().GetInterfaces();
-                var genericTypeArguments = interfaces.Select(i => i.GenericTypeArguments);
-
-                return genericTypeArguments
-                    .Any(genericType => genericType.Any(generic => generic == type));
-            });
-    }
 }
