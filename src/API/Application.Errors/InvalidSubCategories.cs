b97e6c5530375908a09b809a11479a611281f0de
Patrick Ratzow <patrickratzow@hotmail.dk>
2022-01-05 13:14:40 +0100

    refactor(api): use record struct where appropriate

--- src/API/Application.Errors/InvalidSubCategories.cs
+++ src/API/Application.Errors/InvalidSubCategories.cs
@@ -2,7 +2,7 @@
 
 namespace Commentor.GivEtPraj.Application.Errors;
 
-public record InvalidSubCategories : IValidationError
+public readonly record struct InvalidSubCategories : IValidationError
 {
     public string ErrorMessage => $"At least one sub category is invalid";
 }
--- src/API/Application.Errors/IsAlreadyPreAuthorized.cs
+++ src/API/Application.Errors/IsAlreadyPreAuthorized.cs
@@ -2,7 +2,7 @@
 
 namespace Commentor.GivEtPraj.Application.Errors;
 
-public record IsAlreadyPreAuthorized(Guid Id) : IAlreadyExistsError
+public readonly record struct IsAlreadyPreAuthorized(Guid Id) : IAlreadyExistsError
 {
     public string ErrorMessage => $"The Id {Id} is already authorized";
 }
